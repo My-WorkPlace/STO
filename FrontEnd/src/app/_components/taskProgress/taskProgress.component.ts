15a5b602e68e418bd7ecc7301b3ac51790de6c63
Vova <vovafalkon@gmail.com>
2020-08-05 20:17:18 +0300

    asdawD

---
+++ FrontEnd/src/app/_components/taskProgress/taskProgress.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-taskProgres',
+  templateUrl: './taskProgres.component.html',
+  styleUrls: ['./taskProgres.component.scss']
+})
+export class TaskProgresComponent {
+
+}
